1ad46460861e00668810760814f16eae7a85193f
R0B0GUY <51374105+R0B0GUY@users.noreply.github.com>
2019-07-17 13:50:00 +0100

    inserting + redirect mechanism
    sorted sql problems and now info can be inserted into the database

--- includes/addDevDB.php
+++ includes/addDevDB.php
@@ -8,38 +8,74 @@ $db = new PDO($DBhost, $DBusername, $DBpassword);
 
 $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-$query = 
-"INSERT INTO repairs (
-'receiptNumber', 
-'deviceType', 
-'brand', 
-'dName', 
-'model', 
-'colour', 
-'IMEI', 
-'serial', 
-'pin', 
-'customerName', 
-'customerContact', 
-'customerEmail'
-)
-VALUES (
-'$_POST[receiptNumber]',
-'phone',
-'$_POST[brand]',
-'$_POST[dName]',
-'$_POST[model]',
-'cyan',
-'$_POST[IMEI]',
-'$_POST[serial]',
-'$_POST[pin]',
-'$_POST[customerName]',
-'$_POST[customerContact]',
-'$_POST[customerEmail]'
-)";
+$query = "INSERT INTO `repairs` (
+	`mainIndex`, 
+	`dateCreated`, 
+	`receiptNumber`, 
+	`deviceType`, 
+	`brand`, 
+	`dName`, 
+	`model`, 
+	`colour`, 
+	`IMEI`, 
+	`serial`, 
+	`pin`, 
+	`fault`, 
+	`cost`, 
+	`paid`, 
+	`due`, 
+	`customerName`, 
+	`customerContact`, 
+	`customerEmail`, 
+	`dateComplete`, 
+	`completeBy`, 
+	`collectionDate`, 
+	`notified`, 
+	`comments`
+) VALUES (
+	NULL, 
+	current_timestamp()," . //timestamp is auto gened
+	"'".$_POST['receiptNumber']."',". //rec no
+	"'phone',". //dev type
+	"'".$_POST['brand']."',". //brand
+	"'".$_POST['dName']."',". //dev name
+	"'".$_POST['model']."',". //model no
+	"'black',". //colour
+	"'".$_POST['IMEI']."',". //IMEI
+	"NULL,". //serial no
+	"NULL,". //pin 
+	"'',". //fault
+	"'0',". //cost
+	"'0',". //paid
+	"'0',". //due
+	"'".$_POST['customerName']."',". //cust name
+	"'".$_POST['customerContact']."',". //cust phone
+	"'".$_POST['customerEmail']."',". //cust email
+	"NULL,". //date complete
+	"NULL,". //who completed
+	"'',". //collection date
+	"'',". //notified
+	"'this is a test'". //comments
+")";
 
 $statement = $db -> prepare($query);
 
 $statement -> execute();
 
+
+$query2 ="
+SELECT MAX(mainIndex)
+  FROM repairs
+ ";
+
+ $statement = $db -> prepare($query2);
+
+ $statement -> execute();
+$num = $statement -> fetchAll();
+//var_dump($statement);
+var_dump($num[0][0]);
+
 $statement -> closeCursor();
+
+$redirect = "Location: ../device.php/" . $num[0][0];
+header($redirect);
