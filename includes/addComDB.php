269b54e7af2d7837b4b661a2e107602bb6a644ab
R0B0GUY <51374105+R0B0GUY@users.noreply.github.com>
2019-07-26 14:02:51 +0100

    fixed redirect issue
    fixed back referencing issue

--- includes/addComDB.php
+++ includes/addComDB.php
@@ -6,42 +6,19 @@ if(isset($_POST['submit'])) {
     $db = new PDO($DBhost, $DBusername, $DBpassword);
     $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-   /* $query = "INSERT INTO `comments` (
-        'mainIndex',
-        'commentId',
-        'comment'
-    ) VALUES (".
-        $_POST['currententry'].",".
-        "'NULL',".
-        $_POST['comment'].
-    ")";
-    */
-    $query = "INSERT INTO `comments` (`mainIndex`, `commentId`, `comment`) VALUES ('".$_POST['currententry']."', NULL, '".$_POST['comment']."');";
+    $query = "INSERT INTO `comments` (
+        `mainIndex`, 
+        `commentId`, 
+        `comment`
+    ) VALUES ('".
+        $_POST['currententry']."',
+        NULL,'".
+        $_POST['comment']."'
+    );";
 
     $statement = $db -> prepare($query);
     $statement -> execute();
 }
-$query2 ="
-SELECT MAX(commentId)
-  FROM comments
- ";
 
- $statement = $db -> prepare($query2);
-
- $statement -> execute();
-$num = $statement -> fetchAll();
-$num = $num[0][0];
-
-$query3 ="
-SELECT * FROM comments ORDER BY commentId DESC LIMIT 1,".$num.";";
-
- $statement = $db -> prepare($query3);
-
-$statement -> execute();
-$dvcent = $statement -> fetchAll();
-$dvcent = $dvcent[0][0];
-$statement -> closeCursor();
-
-$redirect = "Location: ../device.php/" . $dvcent; //navigate to mainIndex of the entry
-//var_dump($_POST);
+$redirect = "Location: ../device.php/" . $_POST['currententry']; //navigate to mainIndex of the entry
 header($redirect); //redirect to that new entry page
